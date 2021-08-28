2434d03d513cf5a1a55350e2c298586a34bdd156
Ramindu <raminduw200@gmail.com>
2021-08-28 09:20:03 +0530

    Add text formatting and add option to print the queue

--- sleeping_barber.c
+++ sleeping_barber.c
@@ -1,43 +1,66 @@
-#include "queue.c"
+#include "queue.h"
 #include <stdio.h>
 #include <unistd.h>
-#include <malloc.h>
+#include <stdlib.h>
 
+void printb(char* string_);
 
 int main(){
     int cust_count;
     char* name;
 
     do {
-        printf("Enter how many customers need to send to the barber shop (Enter 0 to exit): ");
+        printb("\t\tEnter how many customers need to send to the barber shop (Enter 0 to exit): ");
         scanf("%d", &cust_count);
 
         char cust_list[cust_count][100];
 
+        // input all customer names
         for (int i = 0; i < cust_count; ++i) {
-            printf("Customer Name : ");
-            scanf("%s", &cust_list[i]);
-        }
-        for (int i = 0; i < cust_count; ++i) {
-            printf("Customer Name : %s\n", cust_list[i]);
+            printf("\t\tCustomer Name : ");
+            scanf("%s", cust_list[i]);
         }
 
+        // Enqueue
         for (int i = 0; i < cust_count; ++i) {
             name = cust_list[i];
-            if (isFull())
-                printf("No chairs left.\n"
-                       "%s customer has left the barber shop...\n", name);
+            if (isFull()) {
+                printf("\n\t\tNo chairs left.\n\t\t");
+                printb(name);
+                printf(" customer has left the barber shop...\n");
+            }
             else
                 enQueue(name);
         }
 
+        printf("\n\t\tBarber woke up...\n\n");
+
         while (!isEmpty()) {
             char* cust = deQueue();
-            printf("%s is cutting his hair...\n", cust);
-            sleep(5);
-            printf("%s left the barber shop...\n", cust);
+
+            printf("\t\t");
+            printb(cust);
+            printf(" is cutting his hair...\n");
+
+            // Sleep - Random number between 7 and 3
+            sleep(rand() % (7+1-3) + 3);
+
+            printf("\t\t");
+            printb(cust);
+            printf(" left the barber shop...\n\n");
+
+            if (!isEmpty())
+                displaySeats();
+            printf("\n");
         }
+
+        printf("\t\tBarber went to sleep... z... z... z...\n\n");
 
     } while (cust_count != 0);
     return 0;
 }
+
+// Method to bold text, errors
+void printb(char* string_){
+    printf("\e[1m%s\e[0m", string_);
+};
